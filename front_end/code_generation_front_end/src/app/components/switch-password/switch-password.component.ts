caefee3ee4b9dec68b072b8bedd106f725ec7c7c
Alexey Shirayev <aleksi.aleksi2014@yandex.ru>
2023-08-03 20:14:04 +0300

    addition componet switch-password and service auth

---
+++ front_end/code_generation_front_end/src/app/components/switch-password/switch-password.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-password',
+  templateUrl: './switch-password.component.html',
+  styleUrls: ['./switch-password.component.scss']
+})
+export class SwitchPasswordComponent {
+
+}
